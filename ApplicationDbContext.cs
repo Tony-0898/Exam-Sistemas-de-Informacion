a34878f6e818ac9a54ea7672a3ca6c524733fb99
Tony-0898 <75226703+Tony-0898@users.noreply.github.com>
2021-06-18 07:33:13 -0600

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Trex.Models;
+
+namespace Trex.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<dep> Departments {get; set;}
+    }
+}
