dbceb1dcdb893e406278300e97bd52a6a599addc
Tony-0898 <75226703+Tony-0898@users.noreply.github.com>
2021-06-18 07:32:09 -0600

    Add files via upload

---
+++ dep.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trex.Models
+{
+    public class dep
+    {
+            public int depId { get; set; }
+
+            public string depNombre { get; set; }
+            public string depApellido { get; set; }
+            public string depCorreo { get; set; }
+            public string depAcompañanteNombre { get; set; }
+            public string depAcompañanteApellido { get; set; }
+            public string depCalle { get; set; }
+            public string depCalle2 { get; set; }
+            public String depCiudad { get; set; }
+            public string depEstado { get; set; }
+            public string depProvincia { get; set; }
+            public int depTelefono { get; set; }
+            public int depWhatsapp { get; set; }
+
+
+
+        
+    }
+}
